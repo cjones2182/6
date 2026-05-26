135b05161cdc4c859985be6c92f1971e4be7dced
Cory Jones <cory@Corys-MacBook-Air.local>
2026-05-26 15:52:29 -0400

    switched to skip final snapshot

--- modules/rds/rds.tf
+++ modules/rds/rds.tf
@@ -6,7 +6,7 @@ resource "aws_db_instance" "main_rds" {
   instance_class       = "db.t3.micro"
   username             = "app_user"
   password             = var.aws_secretsmanager_secret
-  skip_final_snapshot  = false
+  skip_final_snapshot  = true
   vpc_security_group_ids = [var.rds_security_group]
   multi_az = true
   db_subnet_group_name = aws_db_subnet_group.db_subnet_group.name
